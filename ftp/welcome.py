bcd2cda23d2c76d9760ab0bdf2181971cb0f229b
mxcheung <mxcheung@users.noreply.github.com>
2021-05-22 23:39:26 +1000

    Update welcome.py

--- ftp/welcome.py
+++ ftp/welcome.py
@@ -1,25 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat May 22 23:16:21 2021
+Created on Sat May 22 23:37:05 2021
 
 @author: cheungm
 """
 
-   
 #!/usr/bin/python3
 
 import ftplib
 
 with ftplib.FTP('ftp.dlptest.com') as ftp:
-    
-    try:
-        ftp.login('dlpuser','rNrKYTX9g7z3RgJRmxWuGHbeu')
-
-        files = []
-
-        ftp.dir(files.append)
-
-        print(files)
-            
-    except ftplib.all_errors as e:
-        print('FTP error:', e)    
+    print(ftp.getwelcome())
