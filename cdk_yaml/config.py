8ea445f26df256a846ec0a4f826fa334dccc8b62
mxcheung <mxcheung@users.noreply.github.com>
2025-06-27 22:27:45 +1000

    Update config.py

--- cdk_yaml/config.py
+++ cdk_yaml/config.py
@@ -7,7 +7,12 @@ class Environment(Enum):
     DEV = "dev"
     TEST = "test"
     PROD = "prod"
-    
+
+class Region(Enum):
+    AP_SOUTHEAST_2 = "ap-southeast-2"
+    US_WEST_1 = "us-west-1"
+
+
 @dataclass
 class AppConfig:
     name: str
