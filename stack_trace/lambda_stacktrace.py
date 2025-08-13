ca24c4afac690ceba5e1f31b9019c3dc170471df
mxcheung <mxcheung@users.noreply.github.com>
2025-08-14 09:54:37 +1000

    Create lambda_stacktrace.py

---
+++ stack_trace/lambda_stacktrace.py
@@ -0,0 +1,15 @@
+import traceback
+import logging
+import sys
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    try:
+        my_list = [1, 2, 3]
+        print(my_list[5])  # IndexError
+    except Exception:
+        exc_type, exc_value, exc_tb = sys.exc_info()
+        formatted_trace = traceback.format_exception(exc_type, exc_value, exc_tb)
+        logger.error("Pretty Stack Trace:\n%s", "".join(formatted_trace))
