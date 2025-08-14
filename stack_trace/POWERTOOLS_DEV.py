ab8333326693969083406cf53bc23b6fdafe1306
mxcheung <mxcheung@users.noreply.github.com>
2025-08-14 11:29:32 +1000

    Update POWERTOOLS_DEV.py

--- stack_trace/POWERTOOLS_DEV.py
+++ stack_trace/POWERTOOLS_DEV.py
@@ -11,3 +11,22 @@ def lambda_handler(event, context):
         print(my_list[5])
     except Exception:
         logger.exception("Processing failed")
+
+
+
+import traceback
+import sys
+from aws_lambda_powertools import Logger
+
+logger = Logger(service="payment-service")
+
+def lambda_handler(event, context):
+    try:
+        try:
+            1 / 0
+        except ZeroDivisionError as e:
+            raise ValueError("Higher-level error") from e
+    except Exception:
+        exc_type, exc_value, exc_tb = sys.exc_info()
+        logger.error("Full chained traceback:\n%s",
+                     "".join(traceback.format_exception(exc_type, exc_value, exc_tb)))
